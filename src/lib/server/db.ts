6965dbf587a6e00e2f337b447480487b6d114709
Micah <micah.apex@gmail.com>
2026-04-06 21:33:40 -0400

    Fix: update DB URLs that won't work

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 import { env } from '$env/dynamic/private';
 
 const pool = new Pool({
-	connectionString: env.DATABASE_URL || 'postgresql://backyard:backyard@localhost:5432/backyard',
+	connectionString: env.DATABASE_URL,
 	max: 20,
 	idleTimeoutMillis: 30_000,
 	connectionTimeoutMillis: 5_000
